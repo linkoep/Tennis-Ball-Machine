98dea9e9c0392b265b89f51fb8df387e599e17cb
Linus Koepfer <linuskoepfer@Linuss-MacBook-Pro.local>
2017-01-05 22:31:58 -0500

    This version updates to three buttons and a joystick

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -1,120 +1,91 @@
 // Pin setup
-  const int speedIncrease = 2;
-  const int speedDecrease = 3;
-  const int topSpin = 4;
-  const int backSpin = 5;
-  const int XPin = A1;
-  const int YPin = A0;
+  const int increase = 2;
+  const int decrease = 3;
+  const int xPin = A1;
+  const int yPin = A0;
+  const int mode = 4;
 
-  // Button States
-  int speedIncreaseState;
-  int speedDecreaseState;
-  int topSpinState;
-  int backSpinState;
+ // Button States
+  int increaseState;
+  int decreaseState;
+  int modeState;
 
   //Previous button states
-  int lastSpeedIncreaseState = LOW;
-  int lastSpeedDecreaseState = LOW;
-  int lastTopSpinState = LOW;
-  int lastBackSpinState = LOW;
-
- /* //Debounce stuff
-  long DBTSpeedIncrease;
-  long DBTSpeedDecrease;
-  long DBTTopSpin;
-  long DBTBackSpin;
-  long debounceDelay = 50; */
+  int lastIncreaseState = HIGH;
+  int lastDecreaseState = HIGH;
+  int lastModeState = HIGH;
 
   //Joystick States
-  int XValue;
-  int YValue;
+  int xValue;
+  int yValue;
 
+  
 void setup() {
-  pinMode(speedIncrease, INPUT);
-  pinMode(speedDecrease, INPUT);
-  pinMode(topSpin, INPUT);
-  pinMode(backSpin, INPUT);
-  pinMode(XPin, INPUT);
-  pinMode(YPin, INPUT);
+  pinMode(increase, INPUT_PULLUP);
+  pinMode(decrease, INPUT_PULLUP);
+  pinMode(mode, INPUT_PULLUP);
+  pinMode(xPin, INPUT);
+  pinMode(yPin, INPUT);
 
   Serial.begin(9600);
 
 }
+
 void loop() {
-  speedIncreaseState = digitalRead(speedIncrease);
-  speedDecreaseState = digitalRead(speedDecrease);
-  topSpinState = digitalRead(topSpin);
-  backSpinState = digitalRead(backSpin);
-  XValue = analogRead(XPin);
-  YValue = analogRead(YPin);
+  increaseState = digitalRead(increase);
+  decreaseState = digitalRead(decrease);
+  modeState = digitalRead(mode);
+  xValue = analogRead(xPin);
+  yValue = analogRead(yPin);
+
+//_________________________________
+//    Buttons
+//_________________________________
 
  
-  /* The following code checks for a state change, changes the last stored state
-     and sends the corresponding signal to serial if the button was pressed 
-     Commented out sections are for debouncing the input, which currently doesn't work for me*/
-  /*if (speedIncreaseState != lastSpeedIncreaseState) {
-    DBTSpeedIncrease = millis();
-  }
-  if ((millis() - DBTSpeedIncrease) > debounceDelay) { */
-    if (speedIncreaseState != lastSpeedIncreaseState) {
-      lastSpeedIncreaseState = speedIncreaseState;
-      if (lastSpeedIncreaseState == HIGH) {
-        Serial.println("a");
+  if (increaseState != lastIncreaseState) {         //check for a button state change
+    delay(50);                                      //wait 50ms
+    if (increaseState != lastIncreaseState) {       //if state is still changed after 50ms (to avoid bouncing)
+      lastIncreaseState = increaseState;            //change the stored state to the new state
+      if (lastIncreaseState == LOW) {               //if the new stored state is LOW (The button is pressed and its connection to ground brings it down from the normal high state caused by the pulldwon resistor)
+        Serial.println("Increase");                 //send a message to serial stating the button has been pressed
       }
     }
-  //}
-
- // The following does the same for the next button
-  /*if (speedDecreaseState != lastSpeedDecreaseState) {
-    DBTSpeedDecrease = millis();
   }
-  if ((millis() - DBTSpeedDecrease) > debounceDelay) { */
-    if (speedDecreaseState != lastSpeedDecreaseState) {
-      lastSpeedDecreaseState = speedDecreaseState;
-      if (lastSpeedDecreaseState == HIGH) {
-        Serial.println("b");
+
+  if (decreaseState != lastDecreaseState) {         //check for a button state change
+    delay(50);                                      //wait 50ms
+    if (decreaseState != lastDecreaseState) {       //if state is still changed after 50ms (to avoid bouncing)
+      lastDecreaseState = decreaseState;            //change the stored state to the new state
+      if (lastDecreaseState == LOW) {               //if the new stored state is LOW (The button is pressed and its connection to ground brings it down from the normal high state caused by the pulldwon resistor)
+        Serial.println("Decrease");                 //send a message to serial stating the button has been pressed
       }
     }
-  //}
-
-  //And for the next
- /* if (topSpinState != lastTopSpinState) {
-    DBTTopSpin = millis();
   }
-  if ((millis() - DBTTopSpin) > debounceDelay) { */
-    if (topSpinState != lastTopSpinState) {
-      lastTopSpinState = topSpinState;
-      if (lastTopSpinState == HIGH) {
-        Serial.println("c");
+
+  if (modeState != lastModeState) {               //check for a button state change
+    delay(50);                                    //wait 50ms
+    if (modeState != lastModeState) {             //if state is still changed after 50ms (to avoid bouncing)
+      lastModeState = modeState;                  //change the stored state to the new state
+      if (lastModeState == LOW) {                 //if the new stored state is LOW (The button is pressed and its connection to ground brings it down from the normal high state caused by the pulldwon resistor)
+        Serial.println("Mode");                   //send a message to serial stating the button has been pressed
       }
     }
-  //}
-
-  //And for the next
- /* if (backSpinState != lastBackSpinState) {
-    DBTBackSpin = millis();
   }
-  if ((millis() - DBTBackSpin) > debounceDelay) { */
-    if (backSpinState != lastBackSpinState) {
-      lastBackSpinState = backSpinState;
-      if (lastBackSpinState == HIGH) {
-        Serial.println("d");
-      }
-    }
- // }
 
- //Joystick Code Block
-  if (!(XValue < 600 && XValue > 400 && YValue < 600 && YValue > 400)){
-    if(XValue > 600) {
-      Serial.println("right");
-    } else if (XValue < 400) {
+//_________________________________
+//      Joystick
+//_________________________________
+if (!(xValue < 600 && xValue > 400 && yValue < 600 && yValue > 400)){   //Check if any values are not between 400 and 600
+    if(xValue > 600) {                                                  //If the X value is greater than 600 (center)
+      Serial.println("right");                                          //Send a message to serial stating the joystick has moved right
+    } else if (xValue < 400) {                                          //Same thing for the other 3 values
       Serial.println("left");
-    } else if (YValue > 600) {
+    } else if (yValue < 400) {
       Serial.println("up");
-    } else if (YValue < 400) {
+    } else if (yValue > 600) {
       Serial.println("down");
     }
-    delay(100);
+    delay(100);                                                         //10 messages per second should be enough to give a quick response without making the motor move in too small increments
   }
- 
 }
