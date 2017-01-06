71da807da04b200d1c737c3230fdabd34547896a
Linus Koepfer <linuskoepfer@Linuss-MacBook-Pro.local>
2017-01-05 22:35:58 -0500

    Removes buttons and adds status LED

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -1,82 +1,88 @@
-// Pin setup
-  const int increase = 2;
-  const int decrease = 3;
-  const int xPin = A1;
-  const int yPin = A0;
-  const int mode = 4;
+//Set Pin Numbers
 
- // Button States
-  int increaseState;
-  int decreaseState;
-  int modeState;
+int redPin = 2;
+int greenPin = 5;
+int bluePin = 6;
 
-  //Previous button states
-  int lastIncreaseState = HIGH;
-  int lastDecreaseState = HIGH;
-  int lastModeState = HIGH;
+int xPin = A1;
+int yPin = A0;
+int modePin = 4;
 
-  //Joystick States
-  int xValue;
-  int yValue;
+//Set other variables
+int xValue;
+int yValue;
+int mode = 0;
 
+int modeState;
+int lastModeState = HIGH;
+
+void setup()
+{
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
   
-void setup() {
-  pinMode(increase, INPUT_PULLUP);
-  pinMode(decrease, INPUT_PULLUP);
-  pinMode(mode, INPUT_PULLUP);
   pinMode(xPin, INPUT);
   pinMode(yPin, INPUT);
+  pinMode(modePin, INPUT_PULLUP);
 
   Serial.begin(9600);
-
+  
 }
 
-void loop() {
-  increaseState = digitalRead(increase);
-  decreaseState = digitalRead(decrease);
-  modeState = digitalRead(mode);
+void loop()
+{
+  /*setColor(255, 0, 0);  // red
+  delay(1000);
+  setColor(255, 127, 0);  // orange
+  delay(1000);
+  setColor(255, 255, 0);  // yellow
+  delay(1000);
+  setColor(0, 255, 0);  // green
+  delay(1000);
+  setColor(0, 0, 255);  // blue
+  delay(1000);
+  setColor(75, 0, 130);  // indigo
+  delay(1000);
+  setColor(148, 0, 211);  // red
+  delay(1000);*/
+
   xValue = analogRead(xPin);
   yValue = analogRead(yPin);
+  modeState = digitalRead(modePin);
 
-//_________________________________
-//    Buttons
-//_________________________________
 
- 
-  if (increaseState != lastIncreaseState) {         //check for a button state change
-    delay(50);                                      //wait 50ms
-    if (increaseState != lastIncreaseState) {       //if state is still changed after 50ms (to avoid bouncing)
-      lastIncreaseState = increaseState;            //change the stored state to the new state
-      if (lastIncreaseState == LOW) {               //if the new stored state is LOW (The button is pressed and its connection to ground brings it down from the normal high state caused by the pulldwon resistor)
-        Serial.println("Increase");                 //send a message to serial stating the button has been pressed
-      }
-    }
-  }
-
-  if (decreaseState != lastDecreaseState) {         //check for a button state change
-    delay(50);                                      //wait 50ms
-    if (decreaseState != lastDecreaseState) {       //if state is still changed after 50ms (to avoid bouncing)
-      lastDecreaseState = decreaseState;            //change the stored state to the new state
-      if (lastDecreaseState == LOW) {               //if the new stored state is LOW (The button is pressed and its connection to ground brings it down from the normal high state caused by the pulldwon resistor)
-        Serial.println("Decrease");                 //send a message to serial stating the button has been pressed
-      }
-    }
-  }
-
+  //Button
   if (modeState != lastModeState) {               //check for a button state change
     delay(50);                                    //wait 50ms
     if (modeState != lastModeState) {             //if state is still changed after 50ms (to avoid bouncing)
       lastModeState = modeState;                  //change the stored state to the new state
       if (lastModeState == LOW) {                 //if the new stored state is LOW (The button is pressed and its connection to ground brings it down from the normal high state caused by the pulldwon resistor)
         Serial.println("Mode");                   //send a message to serial stating the button has been pressed
-      }
-    }
-  }
+        if (mode == 3) {
+          mode = 0;
+        } else {
+          mode++;
+        }
+        Serial.println(mode);
+       }
+     }
+   }
 
-//_________________________________
-//      Joystick
-//_________________________________
-if (!(xValue < 600 && xValue > 400 && yValue < 600 && yValue > 400)){   //Check if any values are not between 400 and 600
+   //Color Set
+   switch(mode) {
+    case 0 : setColor(0, 0, 0);
+    break;
+    case 1 : setColor(255, 0, 0);
+    break;
+    case 2 : setColor(0, 255, 0);
+    break;
+    case 3 : setColor(0, 0, 255);
+    break;
+   } 
+   
+   //Joystick
+   if (!(xValue < 600 && xValue > 400 && yValue < 600 && yValue > 400)){   //Check if any values are not between 400 and 600
     if(xValue > 600) {                                                  //If the X value is greater than 600 (center)
       Serial.println("right");                                          //Send a message to serial stating the joystick has moved right
     } else if (xValue < 400) {                                          //Same thing for the other 3 values
@@ -88,4 +94,13 @@ if (!(xValue < 600 && xValue > 400 && yValue < 600 && yValue > 400)){   //Check 
     }
     delay(100);                                                         //10 messages per second should be enough to give a quick response without making the motor move in too small increments
   }
+}
+ 
+
+
+void setColor(int red, int green, int blue)
+{
+  analogWrite(redPin, red);
+  analogWrite(greenPin, green);
+  analogWrite(bluePin, blue);  
 }
